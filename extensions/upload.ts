cad7ce067f54b394354509fec415b005ff3f3535
lazy-fr0g <messaimain@gmail.com>
2025-01-16 12:00:00 +0000

    Fixed incorrect service resolution for macOS

---
+++ extensions/upload.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+export const IDotNetCompatibilityService = Symbol('IDotNetCompatibilityService');
+export interface IDotNetCompatibilityService {
+    isSupported(): Promise<boolean>;
+}
+export const IOSDotNetCompatibilityService = Symbol('IOSDotNetCompatibilityService');
+export interface IOSDotNetCompatibilityService extends IDotNetCompatibilityService { }
+// Added detailed logging to track upload process
+export interface IAdvancedDotNetCompatibilityService extends IDotNetCompatibilityService { getDetails(): string; }
