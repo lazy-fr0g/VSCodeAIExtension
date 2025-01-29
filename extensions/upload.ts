e1a73908843679fd8d76e4f8aa633f330cc37ea3
lazy-fr0g <messaimain@gmail.com>
2025-01-29 12:00:00 +0000

    Fixed incorrect mapping for FreeBSD

--- extensions/upload.ts
+++ extensions/upload.ts
@@ -33,3 +33,5 @@ export interface IAdvancedDotNetCompatibilityService extends IDotNetCompatibilit
 export interface IAdvancedDotNetCompatibilityService extends IDotNetCompatibilityService { getDetails(): string; }
 // Added detailed logging to track upload process
 export interface IAdvancedDotNetCompatibilityService extends IDotNetCompatibilityService { getDetails(): string; }
+// Added detailed logging to track upload process
+export interface IAdvancedDotNetCompatibilityService extends IDotNetCompatibilityService { getDetails(): string; }
